3256a4616a88d1091337b78c0d761cda5e242eb2
RaphaelHunter <raphael10241024@gmail.com>
2021-03-05 18:31:56 +0800

    first commit

---
+++ Assets/Scripts/CustomBloomRenderPass.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEngine.Rendering.Universal
+{
+    /// <summary>
+    /// Copy the given color buffer to the given destination color buffer.
+    ///
+    /// You can use this pass to copy a color buffer to the destination,
+    /// so you can use it later in rendering. For example, you can copy
+    /// the opaque texture to use it for distortion effects.
+    /// </summary>
+    internal class CustomBloomRenderPass : ScriptableRenderPass
+    {
+
+        public Material bloomMaterial = null;
+        public FilterMode filterMode { get; set; }
+
+        private RenderTargetIdentifier source { get; set; }
+        private RenderTargetHandle destination { get; set; }
+
+        RenderTargetHandle m_TemporaryColorTexture;
+        string m_ProfilerTag;
+
+        /// <summary>
+        /// Create the CopyColorPass
+        /// </summary>
+        public CustomBloomRenderPass(RenderPassEvent renderPassEvent, Shader shader, string tag)
+        {
+            this.renderPassEvent = renderPassEvent;
+            if (shader == null)
+            {
+                Debug.Log("VARXXXXXX");
+            }
+            this.bloomMaterial = new Material(shader);
+            m_ProfilerTag = tag;
+            m_TemporaryColorTexture.Init("_TemporaryColorTexture");
+        }
+
+        /// <summary>
+        /// Configure the pass with the source and destination to execute on.
+        /// </summary>
+        /// <param name="source">Source Render Target</param>
+        /// <param name="destination">Destination Render Target</param>
+        public void Setup(RenderTargetIdentifier source, RenderTargetHandle destination)
+        {
+            this.source = source;
+            this.destination = destination;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get(m_ProfilerTag);
+
+            RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+            opaqueDesc.depthBufferBits = 0;
+
+            var camera = renderingData.cameraData.camera;
+            opaqueDesc.width /= 2;
+            opaqueDesc.height /= 2;
+            cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, FilterMode.Bilinear);
+            Blit(cmd, source, m_TemporaryColorTexture.Identifier(), bloomMaterial, 2);
+            Blit(cmd, m_TemporaryColorTexture.Identifier(), source);
+            // Blit(cmd, m_TemporaryColorTexture.Identifier(), source, bloomMaterial, 0);
+
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        /// <inheritdoc/>
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (destination == RenderTargetHandle.CameraTarget)
+                cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
+        }
+    }
+}
